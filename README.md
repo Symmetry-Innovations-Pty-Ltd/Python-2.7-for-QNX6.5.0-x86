f24ff021f9f64e85adc4f3e5d057a0463a9af725
SI Admin <info@symmetry.com.au>
2014-11-06 13:02:42 +1100

    initial

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Python-2.7-for-QNX6.5.0-x86
 ===========================
 
-Python 2.7 for QNX6.5.0 x86
+Python 2.7 for QNX6.5.0 x86.
+
+Copyright (c) 2014 Symmetry Innovations Pty Ltd
---
+++ usr/pkg/include/ETL/etl_profile.h
@@ -0,0 +1,139 @@
+/* ETL/etl_profile_.h.  Generated from etl_profile_.h.in by configure.  */
+/* ETL/etl_profile_.h.in.  Generated from configure.ac by autoheader.  */
+
+
+// We want to be autoconf/autoheader friendly, so 
+// if the developer has already included an
+// autoheader-generated configuration script, 
+// we can assume that the user knew what they
+// were doing and let them use their own config.h.
+//#ifndef ETL_TARNAME
+
+
+/* Define to 1 if you have the `CreateThread' function. */
+/* #undef HAVE_CREATETHREAD */
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `kill' function. */
+#define HAVE_KILL 1
+
+/* Define to 1 if you have the `kernel32' library (-lkernel32). */
+/* #undef HAVE_LIBKERNEL32 */
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+/* #undef HAVE_LIBPTHREAD */
+
+/* Define to 1 if you have the `user32' library (-luser32). */
+/* #undef HAVE_LIBUSER32 */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `pipe' function. */
+#define HAVE_PIPE 1
+
+/* Define to 1 if you have the `pthread_create' function. */
+#define HAVE_PTHREAD_CREATE 1
+
+/* Define to 1 if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 1
+
+/* Define to 1 if you have the `pthread_rwlock_init' function. */
+#define HAVE_PTHREAD_RWLOCK_INIT 1
+
+/* Define to 1 if you have the `pthread_yield' function. */
+/* #undef HAVE_PTHREAD_YIELD */
+
+/* Define to 1 if you have the `QueryPerformanceCounter' function. */
+/* #undef HAVE_QUERYPERFORMANCECOUNTER */
+
+/* Define to 1 if you have the <sched.h> header file. */
+#define HAVE_SCHED_H 1
+
+/* Define to 1 if you have the `sched_yield' function. */
+#define HAVE_SCHED_YIELD 1
+
+/* Define to 1 if you have the `sscanf' function. */
+#define HAVE_SSCANF 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/times.h> header file. */
+#define HAVE_SYS_TIMES_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `vasprintf' function. */
+/* #undef HAVE_VASPRINTF */
+
+/* Define to 1 if you have the `vsnprintf' function. */
+#define HAVE_VSNPRINTF 1
+
+/* Define to 1 if you have the `vsprintf' function. */
+#define HAVE_VSPRINTF 1
+
+/* Define to 1 if you have the `vsscanf' function. */
+#define HAVE_VSSCANF 1
+
+/* Define to 1 if you have the <windows.h> header file. */
+/* #undef HAVE_WINDOWS_H */
+
+/* Define to 1 if you have the `__clone' function. */
+/* #undef HAVE___CLONE */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define ETL_BUGREPORT "http://synfig.org/Bugs"
+
+/* Define to the full name of this package. */
+#define ETL_NAME "Extended Template Library"
+
+/* Define to the full name and version of this package. */
+#define ETL_STRING "Extended Template Library 0.04.11"
+
+/* Define to the one symbol short name of this package. */
+#define ETL_TARNAME "ETL"
+
+/* Define to the version of this package. */
+#define ETL_VERSION "0.04.11"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
+/* define if the vsnprintf function is mangled */
+/* #undef vsnprintf */
+
+
+//#endif // ifndef ETL_TARNAME
+
